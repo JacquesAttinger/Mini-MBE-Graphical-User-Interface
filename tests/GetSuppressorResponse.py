9fd3129850dfb7750f55017e67ad9ced27d0f462
pvanvlaa <pvanvlaanderen@uchicago.edu>
2025-11-06 09:39:40 -0600

    Small bugfixes.

--- tests/GetSuppressorResponse.py
+++ tests/GetSuppressorResponse.py
@@ -12,7 +12,10 @@ ser = serial.Serial(
     timeout=1
 )
 
-ser.write(b"GET Supr\r")
-response = ser.readline().decode().strip()
-print(f"Suppressor Response: {response}")
-ser.close()
+# ser.write(b"GET Supr\r")
+# response = ser.readline().decode().strip()
+# print(f"Suppressor Response: {response}")
+# ser.close()
+
+# ser.write(b"SET Supr off\r")
+# ser.close()
